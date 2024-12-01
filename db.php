c00930302406e02d9704e4e9f45896b70eb8404e
MonsterVault <pokerjoker0506@gmail.com>
2024-12-02 00:14:54 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$servername = "localhost";  // Typically localhost for local development
-$username = "root";         // Your MySQL username (default is 'root' for local)
-$password = "";             // Your MySQL password (default is empty for local)
-$dbname = "questvaultdb";  // Replace with your actual database name
+$servername = "darkseagreen-quail-635236.hostingersite.com";  // Typically localhost for local development
+$username = "u645382831_questvaultuser";         // Your MySQL username (default is 'root' for local)
+$password = "6f/Zcr:Y";             // Your MySQL password (default is empty for local)
+$dbname = "u645382831_questvault";  // Replace with your actual database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
